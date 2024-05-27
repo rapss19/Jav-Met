e10d515c6dc42e74a2d56f54881ebcd2ff1a8a77
rapss19 <zkelfi01@gmail.com>
2024-05-27 22:47:41 +0700

    Create 2213010363.MuhammadRafiSalman.Java

---
+++ 2213010363.MuhammadRafiSalman.Java
@@ -0,0 +1,15 @@
+public class MuhammadRafiSalman {
+
+    public static void main(String[] args) {
+        String fullname = "Muhammad Rafi Salman";
+        String nim = "2213010363";
+        String team = "Jav-Met";
+        String role = "Designer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
