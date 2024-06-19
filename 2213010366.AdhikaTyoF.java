8cc3ce4e94b42b2736b4ea8ac11c9651ff6cb197
rapss19 <zkelfi01@gmail.com>
2024-06-19 10:31:37 +0700

    Create 2213010366.AdhikaTyoF.java

---
+++ 2213010366.AdhikaTyoF.java
@@ -0,0 +1,15 @@
+public class AdhikaTyoF {
+
+    public static void main(String[] args) {
+        String fullname = "Adhika Tyo Ferdiansyah";
+        String nim = "2213010366";
+        String team = "Jav-Met";
+        String role = "Progammer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
