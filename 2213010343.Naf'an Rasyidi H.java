297703bcedae207834ddcd73510dd0f380f82961
rapss19 <zkelfi01@gmail.com>
2024-06-19 10:29:28 +0700

    Create 2213010343.Naf'an Rasyidi H.java

---
+++ 2213010343.Naf'an Rasyidi H.java
@@ -0,0 +1,15 @@
+public class NafanRasyidiHartono {
+
+    public static void main(String[] args) {
+        String fullname = "Naf'an Rasyidi Hartono";
+        String nim = "2213010343";
+        String team = "Jav-Met";
+        String role = "Leader";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
